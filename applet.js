fc8972a254650e2997efbf7d8baa93868343c2e2
Derek Webb <dwebb.af@gmail.com>
2016-04-11 23:16:07 -0700

    Settings are now editable by the applet - cron frequency setting added and is editable

--- applet.js
+++ applet.js
@@ -4,6 +4,7 @@ const Util = imports.misc.util;
 const Lang = imports.lang;
 const Cinnamon = imports.gi.Cinnamon;
 const AppletDir = imports.ui.appletManager.appletMeta["rwall"].path;
+const Settings = imports.ui.settings;
 
 function MyApplet(orientation, panel_height, instance_id) {
     this._init(orientation, panel_height, instance_id);
@@ -15,9 +16,48 @@ MyApplet.prototype = {
     _init: function(orientation, panel_height, instance_id) {
         Applet.IconApplet.prototype._init.call(this, orientation, panel_height, instance_id);
 
+        // Set the icon and tooltip of this applet
         this.set_applet_icon_path(AppletDir + '/icon.png');
         this.set_applet_tooltip(_('Interact with rwall'));
         
+        // Setup this applet's settings
+        this.initSettings(instance_id);
+        
+        // Setup the menu
+        this.initMenu(orientation);
+    },
+    
+    // Sets up this applet's settings
+    initSettings: function(instance_id) {
+        this.settings = new Settings.AppletSettings(this, "rwall", instance_id);
+        
+        this.settings.bindProperty(
+            Settings.BindingDirection.IN,       // The binding direction - IN means we only listen for changes from this applet
+            "query-string",                     // The setting key, from the setting schema file
+            "queryString",                      // The property to bind the setting to - in this case it will initialize this.icon_name to the setting value
+            this.onSettingsChange,              // The method to call when this.icon_name has changed, so you can update your applet
+            null);                              // Any extra information you want to pass to the callback (optional - pass null or just leave out this last argument)
+                                 
+        this.settings.bindProperty(
+            Settings.BindingDirection.IN,
+            "resolution-string",
+            "resolutionString",
+            this.onSettingsChange,
+            null);
+            
+        this.settings.bindProperty(
+            Settings.BindingDirection.IN,
+            "cron-frequency-string",
+            "cronFrequencyString",
+            this.onSettingsChange,
+            null);
+            
+        // Start the application with the initial values
+        this.onSettingsChange();
+    },
+    
+    // Sets up this applet's menu
+    initMenu: function(orientation) {
         // The menu manager closes the menu after focus has changed.
         // Without adding the menu to the menu manager, the menu would stay open
         // until the user clicked on the applet again.
@@ -42,6 +82,16 @@ MyApplet.prototype = {
         this.menu.addMenuItem(nextWallpaperMenuItem);
     },
     
+    onSettingsChange: function() {
+        // Update the settings, then turn cron off/on as needed
+        Util.spawnCommandLine(
+            AppletDir + '/bin/rwall settings ' +
+            '"' + this.queryString + '" ' +
+            '"' + this.resolutionString + '" ' +
+            '"' + this.cronFrequencyString + '"'
+        );
+    },
+    
     // Returns true if the cron job is running, false otherwise
     isCronBeingUsed: function() {
         let usingCron = false;
@@ -59,7 +109,7 @@ MyApplet.prototype = {
     
     // Toggles the cron job off/on
     toggleCron: function() {
-        Util.spawnCommandLine(AppletDir + '/bin/rwall toggle-cron');
+        Util.spawnCommandLine(AppletDir + '/bin/rwall cron-toggle');
     },
     
     runRwall: function() {
